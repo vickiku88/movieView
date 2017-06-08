8f2685b17b2ee56868c1844d99a34fd0ee59d08f
victoria ku <victoria.ku@airbnb.com>
2017-06-08 16:28:03 -0700

    first commit

---
+++ MovieCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  movieViewer
+//
+//  Created by victoria_ku on 6/8/17.
+//  Copyright © 2017 victoria_ku. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
